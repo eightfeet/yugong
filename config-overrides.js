febc10d0f8e8984ef9bc7ff8301dc4fc966b9274
eightfeet <bajiao000000@163.com>
2021-02-14 08:52:23 +0800

    first commit

---
+++ config-overrides.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+const lessVariables = require('./config.themeVariable');
+
+const { override, fixBabelImports, addLessLoader, setWebpackPublicPath, addWebpackAlias } = require('customize-cra');
+
+const multipleEntry = require('react-app-rewire-multiple-entry')([
+  {
+    entry: 'src/dashboard/index.ts',
+    outPath: '/dashboard.html'
+  },
+  {
+    entry: 'src/index.tsx',
+    outPath: '/index.html'
+  }
+]);
+
+const config = override(
+    fixBabelImports('import', {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: true, // change importing css to less
+    }),
+    addLessLoader({
+        lessOptions: {
+          javascriptEnabled: true,
+          modifyVars: lessVariables,
+        },
+        additionalData: "@import 'node_modules/antd/lib/style/themes/default.less';"
+    }),
+    setWebpackPublicPath(process.env.REACT_APP_PUBLIC_PATH),
+    addWebpackAlias({
+      "~": path.resolve(__dirname, "./src"),
+    }),
+    multipleEntry.addMultiEntry
+);
+
+module.exports = config;
---
+++ src/dashboard/index.ts
@@ -0,0 +1,6 @@
+
+function landing() {
+    console.log(2222)
+}
+landing()
+export default landing;
