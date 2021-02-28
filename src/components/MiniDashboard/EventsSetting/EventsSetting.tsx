7e28bd9d1ed35dd1727c49f0f30f7c3ee6d4da77
eightfeet <bajiao000000@163.com>
2021-02-28 22:12:51 +0800

    update

--- src/components/MiniDashboard/EventsSetting/EventsSetting.tsx
+++ src/components/MiniDashboard/EventsSetting/EventsSetting.tsx
@@ -25,18 +25,24 @@ const EventsSetting: React.FC<Props> = () => {
   );
   if (!moduleId) return null;
 
-  const onChange = (type: EventEmitterExpose, data: EventEmitterEventData[]) => {
-    console.log(555, type, data)
-  }
+  const onChange = (
+    type: EventEmitterExpose,
+    data: EventEmitterEventData[]
+  ) => {
+    console.log(555, type, data);
+  };
 
   // 当前激活项是否有事件导出，
-  const exposeEvents: EventEmitterExpose[] = require(`~/modules/${type}`).default.exposeEvents;
+  const exposeEvents: EventEmitterExpose[] = require(`~/modules/${type}`)
+    .default.exposeEvents;
   // 无事件导清除当前模块
   if (exposeEvents.length === 0) return null;
   return (
     <div className={s.root}>
       {Object.keys(events).map((eventTypeName) => {
-        const eventType = exposeEvents.filter((element) => element.name === eventTypeName)[0] || {};
+        const eventType =
+          exposeEvents.filter((element) => element.name === eventTypeName)[0] ||
+          {};
         return (
           <EventGroup
             key={eventTypeName}
