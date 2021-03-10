988ce486f536e05afbab98a67a026bffbf7d5f74
eightfeet <bajiao000000@163.com>
2021-03-11 07:33:33 +0800

    update

--- src/components/MiniDashboard/ApiSetting/ApiSetting.tsx
+++ src/components/MiniDashboard/ApiSetting/ApiSetting.tsx
@@ -1,28 +1,84 @@
-import { Col, Row } from 'antd'
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { RootState } from '~/redux/store'
-import s from './ApiSetting.module.less';
+import { Button, Col, Divider, Input, Row, Select, Tooltip } from "antd";
+import React, { useCallback } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "~/redux/store";
+import s from "./ApiSetting.module.less";
+
+const selectSetting = (onChange: any, value: any) => (
+  <Select value={value} onChange={onChange}>
+    <Select.Option value="mode">mode</Select.Option>
+    <Select.Option value="headers">headers</Select.Option>
+    <Select.Option value="credentials">credentials</Select.Option>
+  </Select>
+);
+
+const methodArray = ["GET", "POST", "PUT", "DELETE"];
+const selectMethod = (onChange: any, value: any) => (
+  <Select value={value} onChange={onChange}>
+    {methodArray.map((item) => (
+      <Select.Option key={item} value={item}>
+        {item}
+      </Select.Option>
+    ))}
+  </Select>
+);
 
 const ApiSetting: React.FC = () => {
-    const api = useSelector((state:RootState) => state.activationItem.api)
-    return (
-        <div className={s.root}>
-            {
-                api?.map(item => <div>
-                    <Row gutter={4}>
-                        <Col span={20}>
-                        {item.method}
-                        {item.url}
-                        </Col>
-                        <Col span={4}>
-                        4
-                        </Col>
-                    </Row>
-                </div>)
-            }
-        </div>
-    )
-}
+  const api = useSelector((state: RootState) => state.activationItem.api);
 
-export default ApiSetting
+  const onChangeInput = useCallback((e) => {
+    console.log(e);
+  }, []);
+
+  const onChangeMethod = useCallback((e) => {
+    console.log(e);
+  }, []);
+
+  const onChangeSetting = useCallback((e) => {
+    console.log(e);
+  }, []);
+
+  return (
+    <div className={s.root}>
+      {api?.map((item, index) => (
+        <div key={item.apiId}>
+          <div className={s.divide}>
+            <div className={s.title}>{item.name || "接口名称"}</div>
+          </div>
+          <Row className={s.row} gutter={4}>
+            <Col span={24}>
+              <Input
+                onChange={onChangeInput}
+                addonBefore={selectMethod(onChangeMethod, item.method)}
+                addonAfter={selectSetting(onChangeSetting, "高级设置")}
+                value={item.url}
+              />
+            </Col>
+          </Row>
+          <Row className={s.row} gutter={4}>
+            <Col span={24}>
+              <Button style={{ width: "100%" }}>入参设置</Button>
+            </Col>
+          </Row>
+          <Divider orientation="left" plain>
+            请求结果发布
+          </Divider>
+          <Row gutter={4}>
+            <Col span={12}>
+              <Tooltip title={<div>将Api请求成功结果发布到全局</div>}>
+                <Button style={{ width: "100%" }}>success</Button>
+              </Tooltip>
+            </Col>
+            <Col span={12}>
+              <Tooltip title={<div>将Api请求失败结果发布到全局</div>}>
+                <Button style={{ width: "100%" }}>error</Button>
+              </Tooltip>
+            </Col>
+          </Row>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ApiSetting;
