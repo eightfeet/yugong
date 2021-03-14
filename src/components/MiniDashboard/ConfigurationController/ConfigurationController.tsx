c2d5d77aa3fe28b4b36a9f080df4a72ec3023a56
eightfeet <bajiao000000@163.com>
2021-03-14 13:27:30 +0800

    update

--- src/components/MiniDashboard/ConfigurationController/ConfigurationController.tsx
+++ src/components/MiniDashboard/ConfigurationController/ConfigurationController.tsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import ApiSetting from '../ApiSetting'
-import EventsSetting from '../EventsSetting'
+import React from "react";
+import { Collapse } from "antd";
+import ApiSetting from "../ApiSetting";
+import EventsSetting from "../EventsSetting";
+const { Panel } = Collapse;
 
 const ConfigurationController = () => {
-    return (
-        <div>
-            <EventsSetting />
-            <ApiSetting />
-        </div>
-    )
-}
+  return (
+    <Collapse bordered={false} defaultActiveKey={["1"]}>
+      <Panel header="事件" key="1">
+        <EventsSetting />
+      </Panel>
+      <Panel header="Api" key="2">
+        <ApiSetting />
+      </Panel>
+    </Collapse>
+  );
+};
 
-export default ConfigurationController
+export default ConfigurationController;
