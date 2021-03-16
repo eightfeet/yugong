aedf476e3f19e9ec9d65e9d3d0274b8bc12cb0c1
eightfeet <bajiao000000@163.com>
2021-03-16 21:19:20 +0800

    update

--- src/components/MiniDashboard/Repository/Repository.tsx
+++ src/components/MiniDashboard/Repository/Repository.tsx
@@ -1,95 +1,152 @@
-import { Button, Card, Col, Input, Modal, Row } from "antd";
-import Draggable from "react-draggable";
-import React, { useCallback, useState } from "react";
-import { MODULES } from "~/core/constants";
+import { Button, Card, Col, Input, Modal, Row } from 'antd';
+import Draggable from 'react-draggable';
+import React, { useCallback, useState } from 'react';
+import { MODULES } from '~/core/constants';
+import { v4 as uuidv4 } from 'uuid';
+import { AppDataLayoutItemTypes, AppDataModuleTypes } from '~/types/appData';
+import { useSelector } from 'react-redux';
+import { RootState } from '~/redux/store';
 
 interface ModalType {
-  moduleName: string;
-  title: string;
-  discribe: string;
+    moduleName: AppDataModuleTypes;
+    title: string;
+    discribe: string;
+}
+
+interface ModalTypeIcon extends ModalType {
+    icon: JSX.Element;
 }
 
 const Repository: React.FC = () => {
-  const [pisition] = useState<{ x: number; y: number }>({ x: 0, y: 0 });
-  const [addedModal, setAddedModal] = useState<ModalType>();
-  const [newModalName, setNewModalName] = useState<string>();
+    const [pisition] = useState<{ x: number; y: number }>({ x: 0, y: 0 });
+    const [addedModal, setAddedModal] = useState<ModalType>();
+    const [newModalName, setNewModalName] = useState<string>();
+    const appData = useSelector((state:RootState) => state.appData);
 
-  const createModal = useCallback((moduleName: string, name?: string) => {
-    const module = require(`~/modules/${moduleName}`).default;
-    console.log("新增组件", moduleName, name, module);
-  }, []);
+    const createModal = useCallback(
+        (moduleType: AppDataModuleTypes, name?: string) => {
+            // get module's static Options
+            const module = require(`~/modules/${moduleType}`).default;
+            const moduleId: string = uuidv4();
+            // Add a new item. It must have a unique key!
+            const layout = {
+                i: moduleId,
+                w: 4,
+                h: 4,
+                x: (appData.length * 4) % 12,
+                y: Infinity, // put it at the bottom
+                moved: false,
+                static: false,
+            };
+            const style = module.style || { basic: {} };
+            const content = { text: 1 };
+            const result: AppDataLayoutItemTypes = {
+                moduleName: name || '未标题',
+                moduleId,
+                layout,
+                style,
+                content,
+                type: moduleType,
+            };
+            console.log('新增组件', result);
+        },
+        []
+    );
 
-  const onStopDrag = useCallback(
-    (moduleName: ModalType) => (e: any) => {
-      if (window.innerWidth - e.screenX > 550) {
-        setAddedModal(moduleName);
-      }
-    },
-    [setAddedModal]
-  );
+    const onStopDrag = useCallback(
+        (module: ModalType) => (e: any) => {
+            if (window.innerWidth - e.screenX > 550) {
+                setAddedModal(module);
+            }
+        },
+        [setAddedModal]
+    );
 
-  const onDoubleClick = useCallback(
-    (moduleName: ModalType) => (e: any) => {
-      setAddedModal(moduleName);
-    },
-    [setAddedModal]
-  );
+    const onDoubleClick = useCallback(
+        (module: ModalType) => (e: any) => {
+            setAddedModal(module);
+        },
+        [setAddedModal]
+    );
 
-  const onCreate = useCallback(() => {
-    setAddedModal(undefined);
-    setNewModalName(undefined);
-    if (addedModal?.moduleName) {
-      createModal(addedModal?.moduleName, newModalName || "未命名");
-    }
-  }, [addedModal?.moduleName, createModal, newModalName]);
+    const onCreate = useCallback(() => {
+        setAddedModal(undefined);
+        setNewModalName(undefined);
+        if (addedModal?.moduleName) {
+            createModal(addedModal?.moduleName, newModalName || '未命名');
+        }
+    }, [addedModal?.moduleName, createModal, newModalName]);
 
-  return (
-    <>
-      <Row gutter={[16, 16]}>
-        {MODULES.map((item) => (
-          <Col span={6} key={item.moduleName}>
-            <Draggable position={pisition} onStop={onStopDrag(item)}>
-              <Card onDoubleClick={onDoubleClick(item)} hoverable cover={null}>
-                <Card.Meta
-                  style={{ textAlign: "center" }}
-                  title={<><div style={{fontSize:'40px', color: '#AAA'}}>{item.icon}</div><div>{item.title}</div></>}
-                  description={item.discribe}
-                />
-              </Card>
-            </Draggable>
-          </Col>
-        ))}
-      </Row>
-      <Modal
-        title={`创建${addedModal?.title || ""}(${
-          addedModal?.moduleName || ""
-        })组件`}
-        visible={!!addedModal}
-        footer={null}
-        onCancel={() => setAddedModal(undefined)}
-      >
-        <Row gutter={[16, 16]}>
-          <Col span={3}></Col>
-          <Col span={15}>
-            <Input
-              type="text"
-              value={newModalName}
-              onChange={(e) => setNewModalName(e.target.value || undefined)}
-              placeholder={`请输入${addedModal?.title || ""}(${
-                addedModal?.moduleName || ""
-              })组件的别名`}
-            />
-          </Col>
-          <Col span={6}>
-            <Button type="primary" onClick={onCreate}>
-              确定
-            </Button>
-          </Col>
-        </Row>
-        <br />
-      </Modal>
-    </>
-  );
+    return (
+        <>
+            <Row gutter={[16, 16]}>
+                {MODULES.map((item) => (
+                    <Col span={6} key={item.moduleName}>
+                        <Draggable
+                            position={pisition}
+                            onStop={onStopDrag(item as ModalTypeIcon)}
+                        >
+                            <Card
+                                onDoubleClick={onDoubleClick(
+                                    item as ModalTypeIcon
+                                )}
+                                hoverable
+                                cover={null}
+                            >
+                                <Card.Meta
+                                    style={{ textAlign: 'center' }}
+                                    title={
+                                        <>
+                                            <div
+                                                style={{
+                                                    fontSize: '40px',
+                                                    color: '#AAA',
+                                                }}
+                                            >
+                                                {item.icon}
+                                            </div>
+                                            <div>{item.title}</div>
+                                        </>
+                                    }
+                                    description={item.discribe}
+                                />
+                            </Card>
+                        </Draggable>
+                    </Col>
+                ))}
+            </Row>
+            <Modal
+                title={`创建${addedModal?.title || ''}(${
+                    addedModal?.moduleName || ''
+                })组件`}
+                visible={!!addedModal}
+                footer={null}
+                onCancel={() => setAddedModal(undefined)}
+            >
+                <Row gutter={[16, 16]}>
+                    <Col span={3}></Col>
+                    <Col span={15}>
+                        <Input
+                            type="text"
+                            value={newModalName}
+                            onChange={(e) =>
+                                setNewModalName(e.target.value || undefined)
+                            }
+                            placeholder={`请输入${addedModal?.title || ''}(${
+                                addedModal?.moduleName || ''
+                            })组件的别名`}
+                        />
+                    </Col>
+                    <Col span={6}>
+                        <Button type="primary" onClick={onCreate}>
+                            确定
+                        </Button>
+                    </Col>
+                </Row>
+                <br />
+            </Modal>
+        </>
+    );
 };
 
 export default Repository;
