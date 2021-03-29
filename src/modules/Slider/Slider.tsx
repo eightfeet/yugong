cb891fa1bad563d2bf1363618027e3a25b4237ee
eightfeet <bajiao000000@163.com>
2021-03-29 18:37:44 +0800

    update

--- src/modules/Slider/Slider.tsx
+++ src/modules/Slider/Slider.tsx
@@ -1,31 +1,31 @@
-import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
-import { AppDataElementsTypes } from '~/types/appData';
-import Swiper from 'swiper';
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { AppDataElementsTypes } from "~/types/appData";
+import Swiper from "swiper";
 import SwiperCore, {
-    Navigation,
-    Pagination,
-    Scrollbar,
-    Lazy,
-} from 'swiper/core';
-import 'swiper/swiper-bundle.min.css';
-import EventEmitter from '~/core/EventEmitter';
-import { Modules } from '~/types/modules';
-import Wrapper from '../Wrapper';
-import isUrl from '~/core/helper/isUrl';
-import getResult from '~/core/getDataFromRunningTime';
-import s from './Slider.module.less';
-import useStyles from './Slider.useStyles';
-import staticConstants from './Slider.staticConstants';
-import classNames from 'classnames';
+  Navigation,
+  Pagination,
+  Scrollbar,
+  Lazy,
+} from "swiper/core";
+import "swiper/swiper-bundle.min.css";
+import EventEmitter from "~/core/EventEmitter";
+import { Modules } from "~/types/modules";
+import Wrapper from "../Wrapper";
+import isUrl from "~/core/helper/isUrl";
+import getResult from "~/core/getDataFromRunningTime";
+import s from "./Slider.module.less";
+import useStyles from "./Slider.useStyles";
+import staticConstants from "./Slider.staticConstants";
+import classNames from "classnames";
 
 export interface SliderProps extends AppDataElementsTypes {
-    id: string;
-    eventEmitter: EventEmitter;
+  id: string;
+  eventEmitter: EventEmitter;
 }
 
 interface ImagesType {
-    imageUrl?: string;
-    imageLink?: string;
+  imageUrl?: string;
+  imageLink?: string;
 }
 
 interface Configs {}
@@ -41,128 +41,115 @@ SwiperCore.use([Navigation, Pagination, Scrollbar, Lazy]);
  */
 
 const Slider: Modules<SliderProps> = (props) => {
-    // ===================================获取变量=================================== //
-    const { style, eventEmitter, events = {}, layout, moduleId } = props;
-    // ===================================创建运行时class============================ //
-    const useClass = useStyles(props);
-    // ===================================定义方法=================================== //
-    const mount = useCallback(() => {
-        eventEmitter.emit(events.mount);
-    }, [eventEmitter, events]);
+  // ===================================获取变量=================================== //
+  const { style, eventEmitter, events = {}, layout, moduleId } = props;
+  // ===================================创建运行时class============================ //
+  const useClass = useStyles(props);
+  // ===================================定义方法=================================== //
+  const mount = useCallback(() => {
+    eventEmitter.emit(events.mount);
+  }, [eventEmitter, events]);
 
-    const unmount = useCallback(() => {
-        eventEmitter.emit(events.unmount);
-    }, [eventEmitter, events]);
+  const unmount = useCallback(() => {
+    eventEmitter.emit(events.unmount);
+  }, [eventEmitter, events]);
 
-    const [images, setImages] = useState<ImagesType[]>();
-    const setData = useCallback((imageUrls, imageLinks) => {
-        const data: ImagesType[] = [];
-        imageUrls?.forEach((element: any, index: number) => {
-            data.push({
-                imageUrl: getResult(element),
-                imageLink: getResult(imageLinks[index]),
-            });
-        });
+  const [images, setImages] = useState<ImagesType[]>();
+  const setData = useCallback((imageUrls, imageLinks) => {
+    const data: ImagesType[] = [];
+    imageUrls?.forEach((element: any, index: number) => {
+      data.push({
+        imageUrl: getResult(element),
+        imageLink: getResult(imageLinks[index]),
+      });
+    });
 
-        const result = data.filter((item) => isUrl(item.imageUrl || '')) || [];
-        setImages(result);
-    }, []);
+    const result = data.filter((item) => isUrl(item.imageUrl || "")) || [];
+    setImages(result);
+  }, []);
 
-    const [config, setConfig] = useState<Configs>({});
-    const setSlider = useCallback(
-        (config: Configs) => {
-            if (config) {
-                setConfig(config);
-            }
-        },
-        [setConfig]
-    );
+  const [config, setConfig] = useState<Configs>({});
+  const setSlider = useCallback(
+    (config: Configs) => {
+      if (config) {
+        setConfig(config);
+      }
+    },
+    [setConfig]
+  );
 
-    useEffect(() => {
-        // 页面挂载
-        mount();
-        // 页面卸载
-        return () => {
-            unmount();
-        };
-    }, [eventEmitter, mount, unmount]);
-
-    // ===================================定义组件方法=================================== //
-    //向eventEmitter注册事件，向外公布
-    useMemo(() => {
-        eventEmitter.addEventListener('setData', setData);
-        eventEmitter.addEventListener('setSlider', setSlider);
-    }, [eventEmitter, setData, setSlider]);
-
-    const onClickImg = useCallback(
-        (item) => () => {
-            if (isUrl(item.imageLink)) {
-                window.location.href = item.imageLink;
-            }
-        },
-        []
-    );
-
-    // 初始化组件参数
-    const params = {
-        navigation: {
-            nextEl: `.${useClass.next}`,
-            prevEl: `.${useClass.prev}`,
-        },
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
+  useEffect(() => {
+    // 页面挂载
+    mount();
+    // 页面卸载
+    return () => {
+      unmount();
     };
+  }, [eventEmitter, mount, unmount]);
 
-    useEffect(() => {
-        (window as any).Swiper = Swiper;
-        const swiper = new Swiper('.swiper-container', {
-            pagination: {
-                el: '.swiper-pagination',
-                type: 'bullets',
-                dynamicBullets: true,
-            },
-        });
+  // ===================================定义组件方法=================================== //
+  //向eventEmitter注册事件，向外公布
+  useMemo(() => {
+    eventEmitter.addEventListener("setData", setData);
+    eventEmitter.addEventListener("setSlider", setSlider);
+  }, [eventEmitter, setData, setSlider]);
 
-        return () => {
-            swiper.destroy();
-        };
-    }, [params, layout]);
+  const onClickImg = useCallback(
+    (item) => () => {
+      if (isUrl(item.imageLink)) {
+        window.location.href = item.imageLink;
+      }
+    },
+    []
+  );
 
-    // // 创建组件
-    return (
-        <Wrapper {...props}>
-            <div className={useClass.sliderWrap}>
-                <div
-                    className={classNames(
-                        'swiper-container',
-                        s.swipercontainer
-                    )}
-                >
-                    <div className="swiper-wrapper">
-                        {images?.map((item, index) => (
-                            <div
-                                className={classNames(
-                                    'swiper-slide',
-                                    s.swiperslide,
-                                    useClass.swiperslide
-                                )}
-                                key={`${moduleId}-slideContent-${index}`}
-                                onClick={onClickImg(item)}
-                            >
-                                <img
-                                    src={item.imageUrl || ''}
-                                    alt={`${index}`}
-                                />
-                            </div>
-                        ))}
-                    </div>
-                    <div className="swiper-pagination"></div>
-                </div>
-            </div>
-        </Wrapper>
-    );
+  // 初始化组件参数
+  const params = useCallback(() => {
+    return {
+      navigation: {
+        nextEl: `.${useClass.next}`,
+        prevEl: `.${useClass.prev}`,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    };
+  }, [useClass.next, useClass.prev]);
+
+  useEffect(() => {
+    (window as any).Swiper = Swiper;
+    const swiper = new Swiper(".swiper-container", params());
+    return () => {
+      swiper.destroy();
+    };
+  }, [params, layout]);
+
+  // 创建组件
+  return (
+    <Wrapper {...props}>
+      <div className={useClass.sliderWrap}>
+        <div className={classNames("swiper-container", s.swipercontainer)}>
+          <div className="swiper-wrapper">
+            {images?.map((item, index) => (
+              <div
+                className={classNames(
+                  "swiper-slide",
+                  s.swiperslide,
+                  useClass.swiperslide
+                )}
+                key={`${moduleId}-slideContent-${index}`}
+                onClick={onClickImg(item)}
+              >
+                <img src={item.imageUrl || ""} alt={`${index}`} />
+              </div>
+            ))}
+          </div>
+          <div className="swiper-pagination"></div>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 /**
