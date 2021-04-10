f535f35a772747b7cea79edf7763ce46b5c61656
eightfeet <bajiao000000@163.com>
2021-04-10 16:45:44 +0800

    取之规则

--- src/core/getDataFromSource.ts
+++ src/core/getDataFromSource.ts
@@ -4,10 +4,13 @@ import { AnyObjectType } from "~/types/appData";
 
 /**
  * 从原数据编译占位符 compilePlaceholderFromDataSource
- *
+ * 取值规则，
+ * 1、如果有传入数据源[dataSource]取值数据从数据源取
+ * 2、没有传入数据源时默认从运行时runningTimes取值
+ * 3、强制从运行时取值，通过标示*.
  * @param {string} operationStr 操作字符
  * @param {AnyObjectType} [dataSource] 数据源，默认runningTime
- * @return {string}  返回编译结果
+ * @return {string} 返回编译结果
  */
 export const compilePlaceholderFromDataSource = (data: string, dataSource?: AnyObjectType): string => {
   if (typeof data !== "string") {
@@ -20,12 +23,15 @@ export const compilePlaceholderFromDataSource = (data: string, dataSource?: AnyO
     const key = item.replace(/\{\{(.[\w|\d|-|/|.]+?)\}\}/gm, "$1");
     const runningTimes = store.getState().runningTimes;
     let value;
-    if ( dataSource) {
+    if (!!key.match(/^\*\.+/)?.length) {
+      // force global 强制从运行时取值
+      value = get(runningTimes, key.replace('*.', ''), runningTimes);
+    } else if (dataSource) {
       // 处理内部数据
-      value = get(dataSource, key);
+      value = get(dataSource, key, dataSource);
     } else {
       // 处理运行时数据
-      value = get(runningTimes, key);
+      value = get(runningTimes, key, runningTimes);
     }
     result = result.replace(item, `${value || ""}`);
   });
