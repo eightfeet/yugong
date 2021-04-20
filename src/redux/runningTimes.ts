33aedaebe1ae3ca451c50b235086b438ef20dea4
eightfeet <bajiao000000@163.com>
2021-04-20 23:24:04 +0800

    重置运行时变量

--- src/redux/runningTimes.ts
+++ src/redux/runningTimes.ts
@@ -6,15 +6,19 @@ const parsed = queryString.parse(window.location.search);
 const height = window.innerHeight;
 const width = window.innerWidth;
 
-const windowHeight = {
-    height,
-    half: height/2,
-    quarter: height/4, 
-    oneTenth: height/10, 
-    oneTwentieth: height/20, 
-    oneThirtieth: height/30,
-    oneFourtieth: height/40,
-    oneFiftieth: height/50,
+const size = {
+    WH: height,
+    WW: width,
+    'WW/2': width/2,
+    'WH/2': height/2,
+    'WW/4': width/4,
+    'WH/4': height/4,
+    'WW/6': width/6,
+    'WH/6': height/6,
+    'WW/8': width/8,
+    'WH/8': height/8,
+    'WW/10': width/10,
+    'WH/10': height/10,
 }
 
 interface RunningTimesItem {
@@ -27,7 +31,7 @@ interface RunningTimesItem {
 export const runningTimes = createModel<RootModel>()({
     state: {
         search: parsed,
-        windowHeight,
+        size,
         unit: {
             vw: width/100,
             vh: height/100,
