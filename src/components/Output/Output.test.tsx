3aca848b51a225a94e56f1d31c21417d8335ddc9
eightfeet <bajiao000000@163.com>
2021-04-20 11:02:06 +0800

    update

--- src/components/Output/Output.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Output from './Output';
-
-test('renders learn react link', () => {
-  render(<Output />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
