c90b80c37e21ed20bb71df3506ab89047452df33
eightfeet <bajiao000000@163.com>
2021-04-22 10:55:24 +0800

    update

--- src/modules/Form/Form.tsx
+++ src/modules/Form/Form.tsx
@@ -1,92 +1,105 @@
-import { useCallback, useEffect } from "react";
-import { Controller, useForm } from "react-hook-form";
-import * as Yup from "yup";
-import { yupResolver } from "@hookform/resolvers/yup";
-import ScopedCssBaseline from "@material-ui/core/ScopedCssBaseline";
-import FormControl from "@material-ui/core/FormControl";
-import Grid from "@material-ui/core/Grid";
-import InputLabel from "@material-ui/core/InputLabel";
-import MenuItem from "@material-ui/core/MenuItem";
-import Select from "@material-ui/core/Select";
-import TextField from "./compoments/TextField";
+import { useCallback, useEffect } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import * as Yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import ScopedCssBaseline from '@material-ui/core/ScopedCssBaseline';
+import FormControl from '@material-ui/core/FormControl';
+import Grid from '@material-ui/core/Grid';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import TextField from './compoments/TextField';
 import Checkbox from './compoments/Checkbox';
-import requester from "~/core/fetch";
-import EventEmitter from "~/core/EventEmitter";
-import { AppDataElementsTypes } from "~/types/appData";
-import { Modules } from "~/types/modules";
-import Wrapper from "../Wrapper";
-import s from "./Form.module.less";
+import requester from '~/core/fetch';
+import EventEmitter from '~/core/EventEmitter';
+import { AppDataElementsTypes } from '~/types/appData';
+import { Modules } from '~/types/modules';
+import Wrapper from '../Wrapper';
+import s from './Form.module.less';
 
 export interface FormProps extends AppDataElementsTypes {
-  id: string;
-  eventEmitter: EventEmitter;
+    id: string;
+    eventEmitter: EventEmitter;
 }
 
 const Form: Modules<FormProps> = (props) => {
-  const { eventEmitter, events = {}, api } = props;
+    const { eventEmitter, events = {}, api } = props;
+    // API请求 注意依赖关系
+    useEffect(() => {
+        const apiArguments = api?.find((item) => item.apiId === '');
+        requester(apiArguments || {});
+    }, [api]);
+    // 基本事件
+    useEffect(() => {
+        // 执行挂载事件
+        eventEmitter.emit(events.mount);
+        return () => {
+            // 执行卸载事件
+            eventEmitter.emit(events.unmount);
+        };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
-  // API请求 注意依赖关系
-  useEffect(() => {
-    const apiArguments = api?.find((item) => item.apiId === "");
-    requester(apiArguments || {});
-  }, [api]);
-  // 基本事件
-  useEffect(() => {
-    // 执行挂载事件
-    eventEmitter.emit(events.mount);
-    return () => {
-      // 执行卸载事件
-      eventEmitter.emit(events.unmount);
+    const onSubmit = useCallback((data: any) => {
+        alert(JSON.stringify(data));
+    }, []);
+
+    const defaultValues = {
+        name: '',
+        description: [false, false, false],
     };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
 
-  const onSubmit = useCallback((data: any) => {
-    alert(JSON.stringify(data));
-  }, []);
+    const schema = Yup.object().shape({
+        name: Yup.string()
+            .required('请输入年月日')
+            .min(3, '请输入姓名大于30个字符')
+            .max(64),
+        description: Yup.bool().oneOf([true], '请选择'),
+    });
 
-  const defaultValues = {
-    name: "",
-    description: [],
-  };
+    const { control, handleSubmit, formState, setValue, getValues } = useForm({
+        mode: 'onChange',
+        defaultValues,
+        resolver: yupResolver(schema),
+    });
+    const { errors } = formState;
 
-  const schema = Yup.object().shape({
-    name: Yup.string()
-      .required("请输入年月日")
-      .min(3, "请输入姓名大于30个字符")
-      .max(64),
-    description: Yup.bool().oneOf([true], '请选择'),
-  });
-
-  const { control, handleSubmit, formState } = useForm({
-    mode: "onChange",
-    defaultValues,
-    resolver: yupResolver(schema),
-  });
-  const { errors } = formState;
-
-  return (
-    <Wrapper {...props} maxHeight maxWidth>
-      <ScopedCssBaseline>
-        <form onSubmit={handleSubmit(onSubmit)} style={{ padding: 24 }}>
-          <Grid container spacing={2}>
-            <TextField
-              fullWidth
-              label="姓名"
-              name="name"
-              type="text"
-              control={control}
-              errors={errors.name}
-            />
-            <Checkbox control={control} name="description" label="请选择" errors={errors.description}/>
-          </Grid>
-          <button type="submit" disabled={!formState.isValid}>
-            Submit
-          </button>
-        </form>
-      </ScopedCssBaseline>
-    </Wrapper>
-  );
+    return (
+        <Wrapper {...props} maxHeight maxWidth>
+            <ScopedCssBaseline>
+                <form onSubmit={handleSubmit(onSubmit)} style={{ padding: 24 }}>
+                    <Grid container spacing={2}>
+                        <TextField
+                            fullWidth
+                            label="姓名"
+                            name="name"
+                            type="text"
+                            control={control}
+                            errors={errors.name}
+                        />
+                        <Checkbox
+                            control={control}
+                            options={[{
+                              label: '选项1',
+                              checked: false
+                            },{
+                              label: '选项2',
+                              checked: false
+                            }]}
+                            name="description"
+                            label="请选择"
+                            errors={errors.description}
+                            setValue={setValue as any}
+                            getValues={getValues as any}
+                        />
+                    </Grid>
+                    <button type="submit" disabled={!formState.isValid}>
+                        Submit
+                    </button>
+                </form>
+            </ScopedCssBaseline>
+        </Wrapper>
+    );
 };
 
 /**
@@ -98,14 +111,14 @@ Form.exposeFunctions = [];
  * 发布事件的静态描述
  */
 Form.exposeEvents = [
-  {
-    name: "mount",
-    description: "初始化",
-  },
-  {
-    name: "unmount",
-    description: "卸载",
-  },
+    {
+        name: 'mount',
+        description: '初始化',
+    },
+    {
+        name: 'unmount',
+        description: '卸载',
+    },
 ];
 
 /**
--- src/modules/Form/compoments/Checkbox/Checkbox.tsx
+++ src/modules/Form/compoments/Checkbox/Checkbox.tsx
@@ -1,62 +1,81 @@
-import { FormGroup, Grid, FormControlLabel, FormControl, FormLabel, FormHelperText } from "@material-ui/core";
-import MUiCheckbox, { CheckboxProps } from "@material-ui/core/Checkbox";
-import { useCallback, useState } from "react";
-import { Controller } from "react-hook-form";
-import s from './Checkbox.module.less'
+import {
+    FormGroup,
+    Grid,
+    FormControlLabel,
+    FormControl,
+    FormLabel,
+    FormHelperText,
+} from '@material-ui/core';
+import MUiCheckbox, { CheckboxProps } from '@material-ui/core/Checkbox';
+import { useCallback, useState } from 'react';
+import { Controller } from 'react-hook-form';
+import s from './Checkbox.module.less';
 
 interface CheckboxsProps {
-  control: any;
-  errors?: any;
-  label?: string
-  name: `${string}` | `${string}.${string}` | `${string}.${number}`;
+    control: any;
+    errors?: any;
+    label?: string;
+    name: `${string}` | `${string}.${string}` | `${string}.${number}`;
+    setValue: (name: string, value: boolean) => any;
+    getValues: any;
+    options: {
+        label: string;
+        checked: boolean;
+    }[];
 }
 
-const Checkbox: React.FC<CheckboxsProps & CheckboxProps> = ({
-  control,
-  errors,
-  name,
-  label,
-  ...other
+const Checkbox: React.FC<CheckboxsProps> = ({
+    control,
+    errors,
+    name,
+    label,
+    options
 }) => {
-  const [state, setState] = useState();
 
-  const handleChange = useCallback(
-    (index) => (event: React.ChangeEvent<HTMLInputElement>) => {
-      // setState({ ...state, [event.target.name]: event.target.checked });
-      console.log(index, event.target);
-    },
-    [],
-  );
+    const handleChange = useCallback(
+        (index) => (event: React.ChangeEvent<HTMLInputElement>) => {
+            console.log(1111)
+        },
+        []
+    );
 
-
-  return (
-    <Grid item xs={12}>
-      <Controller
-        control={control}
-        name={name}
-        render={({ field }) => (
-          <FormControl className={s.fieldset} component="fieldset" error={!!errors} {...field}>
-            {label ? <FormLabel className={s.legend} component="legend">{label}</FormLabel> : null}
-            <FormGroup>
-              <FormControlLabel
-                control={<MUiCheckbox checked={true} onChange={handleChange(0)} />}
-                label="Gilad Gray"
-              />
-              <FormControlLabel
-                control={<MUiCheckbox checked={false} onChange={handleChange(1)} />}
-                label="Jason Killian"
-              />
-              <FormControlLabel
-                control={<MUiCheckbox checked={false} onChange={handleChange(2)} />}
-                label="Antoine Llorca"
-              />
-            </FormGroup>
-            <FormHelperText>{errors?.message}</FormHelperText>
-          </FormControl>
-        )}
-      />
-    </Grid>
-  );
+    return (
+        <Grid item xs={12}>
+            <Controller
+                control={control}
+                name={name}
+                render={({ field }) => (
+                    <FormControl
+                        className={s.fieldset}
+                        component="fieldset"
+                        error={!!errors}
+                        {...field}
+                    >
+                        {label ? (
+                            <FormLabel className={s.legend} component="legend">
+                                {label}
+                            </FormLabel>
+                        ) : null}
+                        <FormGroup>
+                            {options.map(({ checked, label }, index) => (
+                                <FormControlLabel
+                                    key={index}
+                                    control={
+                                        <MUiCheckbox
+                                            onChange={handleChange(index)}
+                                            checked={checked}
+                                        />
+                                    }
+                                    label={label}
+                                />
+                            ))}
+                        </FormGroup>
+                        <FormHelperText>{errors?.message}</FormHelperText>
+                    </FormControl>
+                )}
+            />
+        </Grid>
+    );
 };
 
 export default Checkbox;
