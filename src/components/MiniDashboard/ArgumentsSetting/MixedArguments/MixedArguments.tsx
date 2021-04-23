5f144916effbeaa955a90e73aaac92822ce4334e
eightfeet <bajiao000000@163.com>
2021-04-24 00:12:18 +0800

    更新混合数据

--- src/components/MiniDashboard/ArgumentsSetting/MixedArguments/MixedArguments.tsx
+++ src/components/MiniDashboard/ArgumentsSetting/MixedArguments/MixedArguments.tsx
@@ -5,24 +5,26 @@ import "jsoneditor/dist/jsoneditor.css";
 import { useCallback } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { message } from "antd";
-import { CopyOutlined } from "@ant-design/icons";
+import { CopyOutlined, FormOutlined, SaveOutlined } from "@ant-design/icons";
 import Radio from "antd/lib/radio";
 import Button from "antd/lib/button";
-import { AppDataLayoutItemTypes } from "~/types/appData";
+import { AppDataLayoutItemTypes, ArgumentsItem } from "~/types/appData";
+import cloneDeep from "lodash/cloneDeep";
 
 interface Props {
-    onChange: any;
-    typeArguments: any;
-    flexible: any;
+    onChange: (data: ArgumentsItem) => void;
+    typeArguments: ArgumentsItem;
+    flexible: boolean;
 }
 
-const Mixedarguments: React.FC<Props> = ({typeArguments}) => {
+const Mixedarguments: React.FC<Props> = ({typeArguments, onChange}) => {
 
   const [jsonData, setJsonData] = useState<AppDataLayoutItemTypes>();
   const [jsonMode, setJsonMode] = useState<"view" | "code">("view");
 
   useEffect(() => {
-    setJsonData({ ...typeArguments });
+    const result = cloneDeep(typeArguments);
+    setJsonData(result.data || {});
   }, [typeArguments]);
 
   const jsoneditor = useRef<JSONEditor>();
@@ -31,13 +33,19 @@ const Mixedarguments: React.FC<Props> = ({typeArguments}) => {
   const onsubmit = useCallback(() => {
     try {
       var json = jsoneditor.current?.get();
-      if (json) {
-        console.log(json)
+      if (json && onChange instanceof Function) {
+        const result = cloneDeep(typeArguments);
+        result.data = json;
+        jsoneditor.current?.setMode("view");
+        setJsonMode("view");
+        onChange(result)
+        message.success(`${typeArguments.name}已更新！`)
       }
     } catch (e) {
+      message.error('保存失败！JSON数据格式不正确')
       return;
     }
-  }, []);
+  }, [onChange, typeArguments]);
 
   useEffect(() => {
     if (container.current && jsonData) {
@@ -56,8 +64,16 @@ const Mixedarguments: React.FC<Props> = ({typeArguments}) => {
   }, [jsonData, jsonMode]);
 
   const onChangeJsonMode = useCallback((e) => {
-    jsoneditor.current?.setMode(e.target.value);
-    setJsonMode(e.target.value);
+    try {
+        var json = jsoneditor.current?.get();
+        if (json) {
+          jsoneditor.current?.setMode('code');
+          setJsonMode('code');
+        }
+    } catch (error) {
+      console.error(error)
+    }
+    
   }, []);
   
   return (
@@ -74,21 +90,22 @@ const Mixedarguments: React.FC<Props> = ({typeArguments}) => {
             </Button>
           </CopyToClipboard>
           &nbsp;
-          <Radio.Group
-            value={jsonMode}
-            size="small"
-            onChange={onChangeJsonMode}
-          >
-            <Radio.Button value="view">预览</Radio.Button>
-            <Radio.Button value="code">编辑</Radio.Button>
-          </Radio.Group>
-          &nbsp;
+          {jsonMode === "view" ? (
+            <Button
+              size="small"
+              type="primary"
+              onClick={onChangeJsonMode}
+              icon={<FormOutlined alt="编辑JSON"/>}
+            >
+              编辑
+            </Button>
+          ) : null}
           {jsonMode === "code" ? (
             <Button
               size="small"
               type="primary"
               onClick={onsubmit}
-              icon={<CopyOutlined alt="复制到剪切板" />}
+              icon={<SaveOutlined alt="保存JSON" />}
             >
               保存
             </Button>
