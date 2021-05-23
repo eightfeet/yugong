9c434039ccc1b61b2d0c32c0c5d9676ac5a14a71
eightfeet <bajiao000000@163.com>
2021-05-23 15:06:57 +0800

    update

--- src/components/MiniDashboard/Presetting/Presetting.tsx
+++ src/components/MiniDashboard/Presetting/Presetting.tsx
@@ -1,5 +1,5 @@
 import { Col, Input, PageHeader, Row, Tooltip } from "antd";
-import React, { useMemo } from "react";
+import React, { useCallback, useMemo } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "~/redux/store";
 import { ExposeFunctions, Modules } from "~/types/modules";
@@ -23,12 +23,14 @@ const Presetting: React.FC<Props> = ({}) => {
     (state: RootState) => state.activationItem
   );
 
+  const module: Modules<any> = useMemo(
+    () => (!!type ? require(`~/modules/${type}`).default : {}),
+    [type]
+  );
 
-  const module:Modules<any> = useMemo(() => !!type ? require(`~/modules/${type}`).default : {}, [type])
-
-  if (!moduleId) {
-      return null;
-  }
+  const onChange = useCallback((index: number, type: string, value: any) => {
+    console.log(index, type, value);
+  }, []);
 
   // 获取当前运行时模块已配置的内置方法
   const setFunctions = events?.mount || [];
@@ -36,71 +38,105 @@ const Presetting: React.FC<Props> = ({}) => {
   // 获取当前模块类导出的内置方法，
   const exposeFunctions: ExposeFunctions[] = module.exposeFunctions || [];
 
+  /**
+   * 合并三块数据、组件内部，运行时、以及编辑后的数据
+   * 组件内部数据是最全面的预设编辑数据，所以以他为预设的配置显示基准
+   * 而最完整的预设编辑数据的初始值包含内组件部默认数据，运行时mount事件中包含的数据，而运行时数据需要剔除非组件内部属性数据，
+   * 编辑完成后再剔除未配置的数据把配置好的数据返回到运行时数据中
+   */
 
-  
+  // step1、设置编辑前数据
+  // 运行时mount数据剔除非当前模块数据
+  // todo检查元素参数为何为空！！！
+  const getData = useCallback(
+      () => {
+        const result: ExposeFunctions[] = [...exposeFunctions].map((item => {
+            console.log(item);
+            setFunctions.some(setItem => {
+                const [, fun] = setItem.name.split('/');
+                if (fun === item.name && item.arguments?.length) {
+                    item.arguments = setItem.arguments;
+                    return true;
+                }
+                return false;
+            })
+            return item;
+        })) || [];
+        return result;
+      },
+      [exposeFunctions, setFunctions],
+  )
+
+  const runningData = getData();
+
+  if (!moduleId) {
+    return null;
+  }
+
   return (
     <div>
-      {exposeFunctions.map((item, index) => !!item.arguments?.length ? (
-        <div key={index} className={s.item}>
+      {runningData.map((item, index) =>
+        !!item.arguments?.length ? (
+          <div key={index} className={s.item}>
             <PageHeader title={item.description} />
-          {item.arguments?.map((argItem, argIndex) => (
-            <Row className={s.row} key={argIndex} gutter={10}>
-              <Col span={5} className={s.label}>
-              <Tooltip
-                    placement="topRight"
-                    title={argItem.describe}
-                >
-                {argItem.name}
-                </Tooltip>
-              </Col>
-              <Col span={19}>
-                {argItem.type === "number" ||
-                argItem.type === "string" ||
-                argItem.type === "runningTime" ? (
-                  <Input
-                    onChange={() => console.log()}
-                    placeholder={`请输入值,${argItem.describe || ""}`}
-                    value={argItem.data}
-                    type="text"
-                    suffix={!!argItem.html ? <HtmlSuffix /> : null}
-                  />
-                ) : null}
-                {argItem.type === "array" ? (
-                  <ArrayArguments
-                    typeArguments={argItem}
-                    flexible
-                    htmlInput={!!argItem.html}
-                    onChange={() => console.log()}
-                  />
-                ) : null}
-                {argItem.type === "boolean" ? (
-                  <BooleanArguments
-                    typeArguments={argItem}
-                    flexible={false}
-                    onChange={() => console.log()}
-                  />
-                ) : null}
-                {argItem.type === "object" ? (
-                  <ObjectArguments
-                    describe={argItem.describe}
-                    htmlInput={!!argItem.html}
-                    onChange={() =>console.log()}
-                    typeArguments={argItem}
-                    flexible={false}
-                  />
-                ) : null}
-                {argItem.type === "mixed" ? (
-                  <MixedArguments 
-                    onChange={() => console.log()}
-                    typeArguments={argItem}
-                    flexible={false}
-                  />
-                ) : null}
-              </Col>
-            </Row>
-          ))}
-        </div>
-      ) : null)}
+            {item.arguments?.map((argItem, argIndex) => (
+              <Row className={s.row} key={argIndex} gutter={10}>
+                <Col span={5} className={s.label}>
+                  <Tooltip placement="topRight" title={argItem.describe}>
+                    {argItem.name}
+                  </Tooltip>
+                </Col>
+                <Col span={19}>
+                  {argItem.type === "number" ||
+                  argItem.type === "string" ||
+                  argItem.type === "runningTime" ? (
+                    <Input
+                      onChange={(e) =>
+                        onChange(index, argItem.type, {...argItem, data: e.target.value})
+                      }
+                      placeholder={`请输入值,${argItem.describe || ""}`}
+                      value={argItem.data}
+                      type="text"
+                      suffix={!!argItem.html ? <HtmlSuffix /> : null}
+                    />
+                  ) : null}
+                  {argItem.type === "array" ? (
+                    <ArrayArguments
+                      typeArguments={argItem}
+                      flexible
+                      htmlInput={!!argItem.html}
+                      onChange={(value) => onChange(index, argItem.type, value)}
+                    />
+                  ) : null}
+                  {argItem.type === "boolean" ? (
+                    <BooleanArguments
+                      typeArguments={argItem}
+                      flexible={false}
+                      onChange={(value) => onChange(index, argItem.type, value)}
+                    />
+                  ) : null}
+                  {argItem.type === "object" ? (
+                    <ObjectArguments
+                      describe={argItem.describe}
+                      htmlInput={!!argItem.html}
+                      onChange={(value) => onChange(index, argItem.type, value)}
+                      typeArguments={argItem}
+                      flexible={false}
+                    />
+                  ) : null}
+                  {argItem.type === "mixed" ? (
+                    <MixedArguments
+                      onChange={(value) => onChange(index, argItem.type, value)}
+                      typeArguments={argItem}
+                      flexible={false}
+                    />
+                  ) : null}
+                </Col>
+              </Row>
+            ))}
+          </div>
+        ) : null
+      )}
     </div>
   );
 };
