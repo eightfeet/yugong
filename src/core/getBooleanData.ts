2883dc22d9dbe0006f0b03c6c32d9c6ea884150e
eightfeet <bajiao000000@163.com>
2021-05-28 23:21:12 +0800

    update

--- src/core/getBooleanData.ts
+++ src/core/getBooleanData.ts
@@ -1,7 +1,9 @@
 import { ArgumentsBoolean } from "~/types/appData";
-
+import { compilePlaceholderFromDataSource as getResult } from "./getDataFromSource";
 /**
+ * 
  * boolean 值设计为一个比较的概念，左右参数和比较符号，返回boolean值
+ * comparableAverageA & comparableAverageB允许从运行时里面取值
  * @param comparableAverageA 比较参数1
  * @param comparableAverageB 比较参数2
  * @param method 比较方法
@@ -12,19 +14,21 @@ const getBooleanData = ({
   comparableAverageB,
   method,
 }: ArgumentsBoolean["data"]) => {
-  const Left = comparableAverageA;
-  const Right = comparableAverageB;
+  const Left = getResult(comparableAverageA);
+  const Right = getResult(comparableAverageB);
   switch (method) {
     case "===":
       return Left === Right;
     case "!==":
       return Left !== Right;
     case ">=":
-      return Left >= Right;
+      return (Number(Left) || Left) >= (Number(Right) || Right);
     case "<":
-      return Left < Right;
+      return (Number(Left) || Left) < (Number(Right) || Right);
+    case ">":
+      return (Number(Left) || Left) > (Number(Right) || Right);
     case "<=":
-      return Left <= Right;
+      return (Number(Left) || Left) <= (Number(Right) || Right);
     case "==":
       // eslint-disable-next-line eqeqeq
       return Left == Right;
