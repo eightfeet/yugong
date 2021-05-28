84e2a74c59d4e0a6e59147ac49f3f7bce36d91fc
eightfeet <bajiao000000@163.com>
2021-05-28 17:05:46 +0800

    update

--- src/components/MiniDashboard/ArgumentsSetting/ArrayArguments/ArrayArguments.tsx
+++ src/components/MiniDashboard/ArgumentsSetting/ArrayArguments/ArrayArguments.tsx
@@ -1,100 +1,202 @@
-import { Button, Col, Input, Row } from "antd";
-import React, { useCallback, useEffect, useState } from "react";
-import { MinusOutlined, PlusOutlined } from "@ant-design/icons";
-import { ArgumentsItem } from "~/types/appData";
-import s from "./ArrayArguments.module.less";
-import HtmlSuffix from "../HtmlSuffix";
+import { Button, Col, Input, Row } from 'antd';
+import React, { useCallback, useEffect, useState } from 'react';
+import { MinusOutlined, PlusOutlined } from '@ant-design/icons';
+import { ArgumentsItem } from '~/types/appData';
+import s from './ArrayArguments.module.less';
+import HtmlSuffix from '../HtmlSuffix';
+import {
+    SortableContainer,
+    SortableElement,
+    SortableHandle,
+} from 'react-sortable-hoc';
+import MoveIcon from './MoveIcon';
+import classNames from 'classnames';
+import arrayMove from 'array-move';
+
+const DragHandle = SortableHandle(() => (
+    <span className={s.icon}>
+        <MoveIcon />
+    </span>
+));
+
+interface SortableItemProps {
+    value: string | number | readonly string[] | undefined;
+    htmlInput: boolean;
+    index: number;
+    onChange: (index: number, value: string | null | undefined) => void;
+    onMinus: (index: number) => void;
+    describe: string;
+}
+
+const SortableItem = SortableElement(
+    ({
+        value,
+        htmlInput,
+        index,
+        onChange,
+        onMinus,
+        describe,
+    }: SortableItemProps) => {
+        return (
+            <Row className={classNames(s.row, 'arrayarguments')} gutter={4} key={index}>
+                <Col span={22}>
+                    <DragHandle />
+                    <Input
+                        onChange={(e) => onChange(index, e.target.value)}
+                        prefix={<div className={s.prefix}>{index}</div>}
+                        suffix={htmlInput ? <HtmlSuffix /> : null}
+                        placeholder={`请输入值${describe || ''}`}
+                        type="text"
+                        value={value}
+                    />
+                </Col>
+                <Col span={2} className={s.btn}>
+                    {true ? (
+                        <Button
+                            size="small"
+                            onClick={() => onMinus(index)}
+                            icon={<MinusOutlined />}
+                        />
+                    ) : null}
+                </Col>
+            </Row>
+        );
+    }
+);
+
+interface SortableContainerProps {
+    data: any[];
+    onChange: (index: number, value: string | null | undefined) => void;
+    onMinus: (index: number) => void;
+    htmlInput: boolean;
+    describe: string;
+}
+
+const SortableList = SortableContainer(
+    ({
+        data,
+        onChange,
+        onMinus,
+        htmlInput,
+        describe,
+    }: SortableContainerProps) => {
+        return (
+            <div>
+                {data.map((item: any, index: number) => (
+                    <SortableItem
+                        key={index}
+                        index={index}
+                        value={item}
+                        htmlInput={htmlInput}
+                        onChange={onChange}
+                        onMinus={onMinus}
+                        describe={describe}
+                    />
+                ))}
+            </div>
+        );
+    }
+);
 
 interface Props {
-  typeArguments: ArgumentsItem;
-  htmlInput?: boolean;
-  flexible: boolean;
-  describe?: string;
-  onChange: (data: ArgumentsItem) => void;
+    typeArguments: ArgumentsItem;
+    htmlInput?: boolean;
+    flexible: boolean;
+    describe?: string;
+    onChange: (data: ArgumentsItem) => void;
 }
 
 interface anyObj {
-  [keys: string]: any;
+    [keys: string]: any;
 }
 
 const ArrayArguments: React.FC<Props> = ({
-  typeArguments,
-  flexible,
-  onChange,
-  htmlInput,
-  describe
+    typeArguments,
+    flexible,
+    onChange,
+    htmlInput,
+    describe,
 }) => {
-  const [argumentsState, setArgumentsState] = useState<ArgumentsItem>();
-  useEffect(() => {
-    setArgumentsState(typeArguments);
-  }, [typeArguments]);
+    const [argumentsState, setArgumentsState] = useState<ArgumentsItem>();
+    useEffect(() => {
+        setArgumentsState(typeArguments);
+    }, [typeArguments]);
 
-  const onChangeValue = useCallback(
-    (index: number) => (e: any) => {
-      const result: anyObj = { ...argumentsState };
-      result.data[index] = e.target.value;
-      if (onChange instanceof Function) {
-        onChange(result as ArgumentsItem);
+    const onChangeValue = useCallback(
+        (index: number, value: any) => {
+            const result: anyObj = { ...argumentsState };
+            result.data[index] = value;
+            if (onChange instanceof Function) {
+                onChange(result as ArgumentsItem);
+            }
+            setArgumentsState(result as ArgumentsItem);
+        },
+        [argumentsState, onChange]
+    );
+
+    const onAddKey = useCallback(() => {
+        const result: anyObj = { ...argumentsState };
+        if (!Array.isArray(result.data)) {
+            result.data = [];
+        }
+
+        result.data.push(undefined);
+
+        if (onChange instanceof Function) {
+            onChange(result as ArgumentsItem);
+        }
+        setArgumentsState(result as ArgumentsItem);
+    }, [argumentsState, onChange]);
+
+    const onMinus = useCallback(
+        (index: number) => {
+            const result: anyObj = { ...argumentsState };
+            result.data = result.data.filter(
+                (_: any, ind: number) => ind !== index
+            );
+            if (onChange instanceof Function) {
+                onChange(result as ArgumentsItem);
+            }
+            setArgumentsState(result as ArgumentsItem);
+        },
+        [argumentsState, onChange]
+    );
+
+    const onSort = useCallback(
+      ({oldIndex, newIndex}) => {
+        const result: anyObj = { ...argumentsState };
+        arrayMove(result.data, oldIndex, newIndex);
+        if (onChange instanceof Function) {
+          onChange(result as ArgumentsItem);
       }
       setArgumentsState(result as ArgumentsItem);
-    },
-    [argumentsState, onChange]
-  );
+      },
+      [argumentsState, onChange],
+    )
 
-  const onAddKey = useCallback(() => {
-    const result: anyObj = { ...argumentsState };
-    if (!Array.isArray(result.data)) {
-      result.data = []
-    }
-
-    result.data.push(undefined)
-    
-    if (onChange instanceof Function) {
-      onChange(result as ArgumentsItem);
-    }
-    setArgumentsState(result as ArgumentsItem);
-  }, [argumentsState, onChange]);
-
-  const onMinus = useCallback((index: number) => () => {
-    const result: anyObj = { ...argumentsState };
-    result.data = result.data.filter((_: any,ind:number) => (ind !== index))
-    if (onChange instanceof Function) {
-      onChange(result as ArgumentsItem);
-    }
-    setArgumentsState(result as ArgumentsItem);
-  }, [argumentsState, onChange]);
-
-  const data: any = argumentsState?.data || [];
-  return (
-    <>
-      {true ? (
-        <Row className={s.toolbar} gutter={4}>
-          <Col span={4}>
-            <Button onClick={onAddKey} icon={<PlusOutlined  />}>增加</Button>
-          </Col>
-        </Row>
-      ) : null}
-      {data.map((item: any, index: number) => (
-        <Row className={s.row} gutter={4} key={index}>
-          <Col span={22}>
-            <Input
-              onChange={onChangeValue(index)}
-              prefix={<div className={s.prefix}>{index}</div>}
-              suffix={htmlInput ? <HtmlSuffix /> : null}
-              placeholder={`请输入值${describe || ''}`}
-              type="text"
-              value={item}
-            />
-          </Col>
-          <Col span={2} className={s.btn}>
+    const data: any = argumentsState?.data || [];
+    return (
+        <>
             {true ? (
-              <Button size="small" onClick={onMinus(index)} icon={<MinusOutlined />} />
+                <Row className={s.toolbar} gutter={4}>
+                    <Col span={4}>
+                        <Button onClick={onAddKey} icon={<PlusOutlined />}>
+                            增加
+                        </Button>
+                    </Col>
+                </Row>
             ) : null}
-          </Col>
-        </Row>
-      ))}
-    </>
-  );
+            <SortableList
+                onSortEnd = {onSort}
+                describe={describe || ''}
+                useDragHandle
+                data={data}
+                htmlInput={!!htmlInput}
+                onChange={onChangeValue}
+                onMinus={onMinus}
+            />
+        </>
+    );
 };
 
 export default ArrayArguments;
