4d2f65bee1b598ab411f74dd108f8a00a0d3c3d8
eightfeet <bajiao000000@163.com>
2021-06-27 15:00:41 +0800

    update

---
+++ creatModule.sh
@@ -0,0 +1,11 @@
+NAME=$1
+PATHUrl="./src/modules/"
+if [ -z $NAME ]; then
+    echo '缺少模块名称参数 npm run createModule [ModuleName]'
+    exit
+fi
+mkdir $PATHUrl$NAME;
+touch $PATHUrl$NAME/index.ts;
+touch $PATHUrl$NAME/$NAME.config.ts;
+touch $PATHUrl$NAME/$NAME.useStyles.ts;
+touch $PATHUrl$NAME/$NAME.tsx;
