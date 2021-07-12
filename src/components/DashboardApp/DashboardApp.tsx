3ddc781882c4e561a1ac5341019cdba1dd21a75a
eightfeet <bajiao000000@163.com>
2021-07-12 11:45:34 +0800

    update

--- src/components/DashboardApp/DashboardApp.tsx
+++ src/components/DashboardApp/DashboardApp.tsx
@@ -1,26 +1,31 @@
-import React from "react";
-import { HashRouter as Router, Route } from "react-router-dom";
-import Layout from "~/components/DashboardApp/Layout";
-import Responsive from "~/components/Responsive";
-import Home from "~/dashboardRouter/Home";
-import PageLogin from "~/dashboardRouter/PageLogin";
-import PageRegister from "~/dashboardRouter/PageRegister";
+import React from 'react';
+import {
+    HashRouter as Router,
+    Route,
+    Switch,
+} from 'react-router-dom';
+import Layout from '~/components/DashboardApp/Layout';
+import Responsive from '~/components/Responsive';
+import Home from '~/dashboardRouter/Home';
+import PageLogin from '~/dashboardRouter/PageLogin';
+import PageRegister from '~/dashboardRouter/PageRegister';
 
 interface Props {}
 
 const Dashboardapp: React.FC<Props> = () => {
-  return (
-    <Router>
-      <Layout>
-        <Route path="/" exact component={Home} />
-        <Route path="/login" component={PageLogin} />
-        <Route path="/register" component={PageRegister} />
-        <Route path="/project" component={Responsive} />
-        <Route path="/404" component={() => <div>找不到页面</div>} />
-        <Route path="*" redirect='/404' />
-      </Layout>
-    </Router>
-  );
+    return (
+        <Router>
+            <Layout>
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/login" component={PageLogin} />
+                    <Route path="/register" component={PageRegister} />
+                    <Route path="/project" component={Responsive} />
+                    <Route path="*" component={() => <div>404找不到页面</div>} />
+                </Switch>
+            </Layout>
+        </Router>
+    );
 };
 
 export default Dashboardapp;
