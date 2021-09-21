cbaf4b180a721c7b504f6207335bf3a813c34680
eightfeet <bajiao000000@163.com>
2021-09-21 20:22:22 +0800

    event与runningtime

--- src/components/MiniDashboard/MarkdownModalDoc/MarkdownModalDoc.tsx
+++ src/components/MiniDashboard/MarkdownModalDoc/MarkdownModalDoc.tsx
@@ -1,38 +1,45 @@
 import { Modal } from "antd";
 import React, { useCallback, useEffect } from "react";
 import useMarked from "~/hooks/useMarked";
-import s from './MarkdownModalDoc.module.less'
+import s from "./MarkdownModalDoc.module.less";
 
 interface Props {
-    visible: boolean;
-    moduleName?: string;
-    onCancel: () => void;
+  visible: boolean;
+  moduleName?: string;
+  onCancel: () => void;
 }
 
-const MarkdownModalDoc: React.FC<Props> = ({ visible, moduleName, onCancel }) => {
-    const [helper, setHelper ] = useMarked();
+const MarkdownModalDoc: React.FC<Props> = ({
+  visible,
+  moduleName,
+  onCancel,
+}) => {
+  const [helper, setHelper] = useMarked();
 
-  const getMd = useCallback(async (name?: string) => {
+  const getMd = useCallback(
+    async (name?: string) => {
       if (!moduleName) {
-            setHelper('');
-          return;
+        setHelper("");
+        return;
       }
-    let text: string = `<h3>找不到${name}文档</h3>请联系模块作者补充`;
-    try {
-      const file = await import(`~/modules/${name}/README.md`);
-      const response = await fetch(file.default);
-      text = await response.text();
-    } catch (error) {
-      console.warn(error);
-    }
-    setHelper(text);
-  }, [moduleName, setHelper]);
+      let text: string = `<h3>找不到${name}文档</h3>请联系模块作者补充`;
+      try {
+        const file = await import(`~/modules/${name}/README.md`);
+        const response = await fetch(file.default);
+        text = await response.text();
+      } catch (error) {
+        console.warn(error);
+      }
+      setHelper(text);
+    },
+    [moduleName, setHelper]
+  );
 
   useEffect(() => {
-      if (visible) {
-        getMd(moduleName)
-      }
-  }, [getMd, moduleName, visible])
+    if (visible) {
+      getMd(moduleName);
+    }
+  }, [getMd, moduleName, visible]);
 
   return (
     <>
