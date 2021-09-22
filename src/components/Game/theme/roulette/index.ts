3d5beec012c480a003ab9d20a42e60abd9a3f47b
eightfeet <bajiao000000@163.com>
2021-09-22 11:16:31 +0800

    修改转盘抽奖默认皮肤

--- src/components/Game/theme/roulette/index.ts
+++ src/components/Game/theme/roulette/index.ts
@@ -1,45 +1,13 @@
 import theme from './../publicTheme';
 
 theme.GameTheme = {
-    wrap: {
-        zIndex: 10,
-    },
-    gameImg: {
-        fontSize: 'normal',
-        height: '40%',
-    },
-    prizeAlias: {
-        fontSize: '0.704em',
-    },
-    needle: {
-        backgroundImage:
-            'url(http://by-health-portal2.oss-cn-beijing.aliyuncs.com/images/20190418150335579.png)',
-        backgroundPosition: 'center center',
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: '100% 100%',
-        width: '8.704em',
-        height: '8.704em',
-        marginLeft: '-4.352em',
-        marginTop: '-4.352em',
-    },
-    lotteryButton: {
-        backgroundImage:
-            'url(http://by-health-portal2.oss-cn-beijing.aliyuncs.com/images/20190418151025381.png)',
-        backgroundPosition: 'center center',
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: '80% 80%',
-    },
-    wheel: {
-        backgroundImage:
-            'url(http://by-health-portal2.oss-cn-beijing.aliyuncs.com/images/20190418142048870.png)',
-        backgroundPosition: 'center center',
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: '100% 100%',
-    },
-    divide: {
-        fontSize: 'normal',
-        display: 'none',
-    },
+    wrap: {},
+    gameImg: {},
+    prizeAlias: {},
+    needle: {},
+    lotteryButton: {},
+    wheel: {},
+    divide: {},
 };
 
 export default theme;
