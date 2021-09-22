fc1ad391a96c725a2ce3365e95c222ccfa46667e
eightfeet <bajiao000000@163.com>
2021-09-22 17:06:44 +0800

    update

--- src/components/Game/theme/dice/index.ts
+++ src/components/Game/theme/dice/index.ts
@@ -2,7 +2,6 @@ import theme from './../publicTheme';
 
 theme.GameTheme = {
     wrap: {
-        backgroundColor: '#efefef',
     },
     // 显示活动信息按钮
     showGameInfoButton: {
@@ -11,38 +10,18 @@ theme.GameTheme = {
     // 活动信息布局
     gameInfoLayout: {},
     // 活动信息外框
-    gameInfoWrap: {
-        paddingTop: '5em',
-    },
+    gameInfoWrap: {},
     // 活动信息奖品单项
-    gameInfoPrizeItem: {
-        height: '10em',
-    },
+    gameInfoPrizeItem: {},
     // 活动信息单项图片
     gameInfoPrizeImg: {},
     // 活动信息奖品名称
-    gameInfoPrizeName: {
-        fontSize: '0.8em',
-        color: 'red',
-    },
+    gameInfoPrizeName: {},
     // 对照标签
-    gameInfoPrizeTag: {
-        color: 'green',
-    },
-    dice: {
-        width: '5em',
-        height: '5em',
-        left: '40vw',
-        top: '40vw',
-    },
-    side: {
-        backgroundColor: '#4caf50',
-        boxShadow: 'inset 0 0 0.4em #0fa40f',
-    },
-    dot: {
-        backgroundColor: '#fff',
-        boxShadow: 'inset 0.2em 0 0.2em rgba(0,0,0,0.1)',
-    },
+    gameInfoPrizeTag: {},
+    dice: {},
+    side: {},
+    dot: {},
     modify: [],
 };
 
--- src/components/Game/theme/slotmachine/index.ts
+++ src/components/Game/theme/slotmachine/index.ts
@@ -2,65 +2,40 @@ import theme from './../publicTheme';
 
 theme.GameTheme = {
     // 外框
-    wrap: {
-        backgroundColor: '#efefef',
-    },
+    wrap: {},
     // 开始按钮
     startButton: {},
+
+    // 游戏奖项图片
+    gamePrizeImg: {
+        maxWidth: '50%'
+    },
+
     // 显示活动信息按钮
     showGameInfoButton: {
-        width: '3em',
-        height: '3em',
-        color: 'rgba(0,0,0,0)',
-        backgroundImage:
-            'url(http://by-health-portal-2019.oss-cn-shenzhen.aliyuncs.com/images/20190808141018604.png)',
-        backgroundSize: '100% 100%',
-        borderRadius: '0.64516rem',
+        display: 'none'
     },
     // 活动信息布局
     gameInfoLayout: {},
     // 活动信息外框
-    gameInfoWrap: {
-        paddingTop: '5em',
-    },
+    gameInfoWrap: {},
     // 活动信息奖品单项
-    gameInfoPrizeItem: {
-        height: '10em',
-    },
+    gameInfoPrizeItem: {},
     // 活动信息单项图片
     gameInfoPrizeImg: {},
     // 活动信息奖品名称
-    gameInfoPrizeName: {
-        fontSize: '0.8em',
-        color: 'red',
-    },
+    gameInfoPrizeName: {},
     // 游戏单项
     gameItem: {},
     // 游戏奖项标题
-    gamePrizeName: {
-        color: 'red',
-        height: '1em',
-    },
-    // 游戏奖项图片
-    gamePrizeImg: {},
+    gamePrizeName: {},
     // 游戏
     game: {},
     // 修饰层
     modify: [
         {
-            top: '-1em',
-            left: '-5em',
-            height: '8em',
-            width: '10em',
-            backgroundColor: 'rgba(255,0,0, 0.5)',
-        },
-        {
-            top: '2em',
-            left: '-2em',
-            height: '8em',
-            width: '20em',
-            backgroundColor: 'rgba(0,255,0, 0.5)',
-        },
+            fontStyle: '',
+        }
     ],
 };
 
