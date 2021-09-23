06abde8fc7f00c7689bd9f7bdb5ee7d0819fe034
eightfeet <bajiao000000@163.com>
2021-09-23 17:05:26 +0800

    更新弹窗

--- src/modules/Lottery/Lottery.useStyles.ts
+++ src/modules/Lottery/Lottery.useStyles.ts
@@ -1,31 +1,48 @@
-import { createUseStyles } from "react-jss";
-import styleCompiler from "~/compiler";
+import { createUseStyles } from 'react-jss';
+import styleCompiler from '~/compiler';
 
-const handlePublicModal = (MID: string, modal: string, style: any) => ({
-  [`& .${MID}_${modal}_wrap .${MID}_${modal}_overlay`]: (styleCompiler(style.dialog_overlay).style || {}),
-  [`& .${MID}_${modal}_wrap .${MID}_${modal}_content_wrap`]: (styleCompiler(style.dialog_content_wrap).style || {}),
-  [`& .${MID}_${modal}_wrap .${MID}_${modal}_content`]: (styleCompiler(style.dialog_content).style || {}),
-  [`& .${MID}_${modal}_wrap .${MID}_${modal}_modules`]: (styleCompiler(style.dialog_modules).style || {}),
-  [`& .${MID}_${modal}_wrap .${MID}_${modal}_article`]: (styleCompiler(style.dialog_article).style || {}),
-  [`& .${MID}_${modal}_wrap .${MID}_${modal}_close`]: (styleCompiler(style.dialog_close).style || {}),
-})
+const handlePublicModal = (MID: string, modal: string, style: any) => {
+    const prefix = `& .${MID}_${modal}_wrap .${MID}_${modal}_`;
+    const publicModal = {
+        [`${prefix}overlay`]: styleCompiler(style.dialog_overlay).style || {},
+        [`${prefix}content_wrap`]: styleCompiler(style.dialog_content_wrap).style || {},
+        [`${prefix}content`]: styleCompiler(style.dialog_content).style || {},
+        [`${prefix}modules`]: styleCompiler(style.dialog_modules).style || {},
+        [`${prefix}article`]: styleCompiler(style.dialog_article).style || {},
+        [`${prefix}close`]: styleCompiler(style.dialog_close).style || {},
+    };
 
-const WH = window.innerHeight;
+    switch (modal) {
+      case 'successmodal':
+        break;
+      case 'failedmodal':
+        break;
+      case 'addressmodal':
+        break;
+      case 'records':
+        break;
+      case 'rules':
+        break;
+      default:
+        break;
+    }
 
-const useStyles = (id: string) => createUseStyles<string, any>({
-      lotteryPublicModal: (style) => ({
-        ...(handlePublicModal(id, 'successmodal', style)),
-        ...(handlePublicModal(id, 'failedmodal', style)),
-        ...(handlePublicModal(id, 'addressmodal', style)),
-        ...(handlePublicModal(id, 'records', style)),
-        ...(handlePublicModal(id, 'rules', style)),
-      }),
+    return publicModal;
+};
 
-      wrap: (style) => {
-        return {
-        };
-      },
+const useStyles = (id: string) =>
+    createUseStyles<string, any>({
+        lotteryPublicModal: (style) => ({
+            ...handlePublicModal(id, 'successmodal', style),
+            ...handlePublicModal(id, 'failedmodal', style),
+            ...handlePublicModal(id, 'addressmodal', style),
+            ...handlePublicModal(id, 'records', style),
+            ...handlePublicModal(id, 'rules', style),
+        }),
 
-});
+        wrap: (style) => {
+            return {};
+        },
+    });
 
 export default useStyles;
