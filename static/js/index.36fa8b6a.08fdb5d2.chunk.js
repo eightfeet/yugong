c2fb355d2acd0eb68f272f9034918fb90cc7b921
eightfeet <bajiao000000@163.com>
2021-09-30 07:13:04 +0800

    publish demo

--- static/js/index.36fa8b6a.08fdb5d2.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpyugong=this.webpackJsonpyugong||[]).push([[2],[],[[1175,5,0,1]]]);
-//# sourceMappingURL=index.36fa8b6a.08fdb5d2.chunk.js.map
--- static/js/main.aa1b4819.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpyugong=this.webpackJsonpyugong||[]).push([[4],[],[[1175,7,0,1]]]);
-//# sourceMappingURL=main.aa1b4819.chunk.js.map
