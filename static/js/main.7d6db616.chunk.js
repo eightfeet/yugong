6c40d1f4018460fecebb3c422ce3a2c79ed49d6b
eightfeet <bajiao000000@163.com>
2021-10-03 18:40:13 +0800

    publish demo

--- static/js/main.7d6db616.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpyugong=this.webpackJsonpyugong||[]).push([[4],[],[[1176,7,0,1]]]);
-//# sourceMappingURL=main.7d6db616.chunk.js.map
---
+++ static/js/main.b8c0aada.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpyugong=this.webpackJsonpyugong||[]).push([[4],[],[[1190,7,0,1]]]);
+//# sourceMappingURL=main.b8c0aada.chunk.js.map
