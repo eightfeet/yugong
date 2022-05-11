c4c32e15086e8ebb80ef2242014f172faaec5ed3
eightfeet <bajiao000000@163.com>
2022-05-11 10:09:06 +0800

    调整表格数据

--- src/modules/Table/Table.Preset/index.tsx
+++ src/modules/Table/Table.Preset/index.tsx
@@ -1,4 +1,4 @@
-import { message, PageHeader, Select } from 'antd';
+import { Input, message, PageHeader, Select } from 'antd';
 import { cloneDeep, isObject, update, throttle, get } from 'lodash';
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { useSelector } from 'react-redux';
@@ -17,23 +17,29 @@ export const tableValuePath = {
   dataType: '[1].arguments[2].data',
   format: '[1].arguments[3].data',
   columWidth: '[1].arguments[4].data',
-}
+};
 
 export type TablePathKeys = keyof typeof tableValuePath;
 
-const TablePreset: React.FC<CustomPersetProps> = ({ runningData, onChange, activationItem }) => {
+const TablePreset: React.FC<CustomPersetProps> = ({
+  runningData,
+  onChange,
+}) => {
   const [disabled, setDisabled] = useState<boolean>(false);
   const [dataSource, setDataSource] = useState<Object[]>([]);
   const { runningTimes } = useSelector((state: RootState) => state);
+  const rtRef = React.useRef<any>(null);
 
   useEffect(() => {
     const path: string = get(runningData, tableValuePath.runningTimes);
     if (path) {
-      const dataSource = (runningTimes[path] || []) as Object[];
+      const dataSource = (get(runningTimes, path) || []) as Object[];
       if (dataSource.length) {
         setDataSource(dataSource);
-      };
-      setDisabled(!dataSource?.length)
+      }
+      setDisabled(!dataSource?.length);
+    } else {
+      setDisabled(true);
     }
   }, [dataSource, runningData, runningTimes]);
 
@@ -42,7 +48,7 @@ const TablePreset: React.FC<CustomPersetProps> = ({ runningData, onChange, activ
       throttle((value) => {
         onChange(value);
       }, 500),
-    [onChange]
+    [onChange],
   );
 
   const onChangeRunningData = useCallback(
@@ -59,57 +65,98 @@ const TablePreset: React.FC<CustomPersetProps> = ({ runningData, onChange, activ
       onChangeDebounce(copyRunningData);
     },
     [onChangeDebounce, runningData],
-  )
+  );
 
   const onChangeRunningTimes = useCallback(
     (e) => {
-      const dataSource = (runningTimes[e] || []) as Object[];
-      if (Array.isArray(dataSource) && dataSource.length && isObject(dataSource[0])) {
-        onChangeRunningData({ runningTimes: e })
+      onChangeRunningData({ runningTimes: e.target.value });
+    },
+    [onChangeRunningData],
+  );
+
+  const onBlurRunningTimes = useCallback(
+    (e) => {
+      const key = e.target.value;
+      const dataSource = (get(runningTimes, key) || []) as Object[];
+      if (
+        Array.isArray(dataSource) &&
+        dataSource.length &&
+        isObject(dataSource[0])
+      ) {
+        onChangeRunningData({ runningTimes: key });
       } else {
-        message.error('请选择合规数组对象 [ { element:string, ... }, ... ]');
+        message.error(
+          '请从runningTimes中选择合规数组对象 [ { element:string, ... }, ... ]',
+        );
+        onChangeRunningData({ runningTimes: '' });
       }
     },
     [onChangeRunningData, runningTimes],
-  )
+  );
+
+  const selectChangeRunningData = useCallback(
+    (e) => {
+      onChangeRunningData({ runningTimes: e });
+      rtRef.current!.focus({
+        cursor: 'end',
+      });
+    },
+    [onChangeRunningData],
+  );
 
   const getDataFromRunningData = useCallback(
     (keys: TablePathKeys[]) => {
       const objRes: { [keys in TablePathKeys]?: any } = {};
-      keys.forEach(key => {
+      keys.forEach((key) => {
         objRes[key] = get(runningData, tableValuePath[key]);
       });
 
       return objRes;
     },
     [runningData],
-  )
+  );
 
   return (
     <>
-      <TableModuleContext.Provider value={{ disabled, setDisabled, dataSource, runningData, onChangeRunningData, getDataFromRunningData }}>
+      <TableModuleContext.Provider
+        value={{
+          disabled,
+          setDisabled,
+          dataSource,
+          runningData,
+          onChangeRunningData,
+          getDataFromRunningData,
+        }}
+      >
         <PageHeader title="表格设置" />
         <LineItem label="数据源">
-          <Select
-            placeholder="请选择"
+          <Input
             value={get(runningData, tableValuePath.runningTimes)}
             onChange={onChangeRunningTimes}
-            style={{ width: "100%" }}
-          >
-            {Object.keys(runningTimes)?.map(
-              (item, index) => (
-                <Option key={index} value={item}>
-                  {item}
-                </Option>
-              )
-            )}
-          </Select>
+            onBlur={onBlurRunningTimes}
+            placeholder="请从runningTimes中选择"
+            ref={rtRef}
+            addonAfter={
+              <Select
+                style={{ width: '100px' }}
+                placeholder="选择"
+                value="运行时"
+                onChange={selectChangeRunningData}
+              >
+                {Object.keys(runningTimes)?.map((item, index) => (
+                  <Option key={index} value={item}>
+                    {item}
+                  </Option>
+                ))}
+              </Select>
+            }
+          />
         </LineItem>
-        <LineItem label='表格列' />
+        <LineItem label="表格列" />
         <SortableTableData />
       </TableModuleContext.Provider>
     </>
-  )
-}
+  );
+};
 
 export default TablePreset;
