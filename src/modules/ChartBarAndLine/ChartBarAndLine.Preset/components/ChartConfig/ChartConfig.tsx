759110e1ccdd702a6a13f6723d1a65738778d24d
eightfeet <bajiao000000@163.com>
2022-08-08 22:52:34 +0800

    调整结构

--- src/modules/ChartBarAndLine/ChartBarAndLine.Preset/components/ChartConfig/ChartConfig.tsx
+++ src/modules/ChartBarAndLine/ChartBarAndLine.Preset/components/ChartConfig/ChartConfig.tsx
@@ -16,31 +16,80 @@ const ChartConfig: React.FC<Props> = ({ }) => {
             <Radio.Button value="vertical">柱状</Radio.Button>
           </Radio.Group>
         </Form.Item>
+        <Form.Item label="背景" name="layout">
+          <Input.Group className={s.groups}>
+            <Form.Item
+              name={['address', 'province']}
+              label="填充"
+              labelCol={{ span: 4 }}
+              wrapperCol={{ span: 18 }}
+            >
+              <Input size="small" placeholder="颜色" />
+            </Form.Item>
+            <Form.Item
+              name={['address', 'province']}
+              label="颜色"
+              labelCol={{ span: 4 }}
+              wrapperCol={{ span: 18 }}
+            >
+              <Input size="small" placeholder="颜色" />
+            </Form.Item>
+          </Input.Group>
+        </Form.Item>
         <Form.Item label="描边">
           <Input.Group className={s.groups}>
             <Form.Item
               name={['address', 'province']}
               label="粗细"
-              labelCol={{span: 4}}
-              wrapperCol={{span: 12}}
+              labelCol={{ span: 4 }}
+              wrapperCol={{ span: 18 }}
             >
               <Input size="small" placeholder="粗细" />
             </Form.Item>
             <Form.Item
               name={['address', 'province']}
-              label="粗细"
-              labelCol={{span: 4}}
-              wrapperCol={{span: 12}}
+              label="颜色"
+              labelCol={{ span: 4 }}
+              wrapperCol={{ span: 18 }}
             >
               <Input size="small" placeholder="粗细" />
             </Form.Item>
             <Form.Item
               name={['address', 'province']}
-              label="粗细"
-              labelCol={{span: 4}}
-              wrapperCol={{span: 8}}
+              label="虚线"
+              labelCol={{ span: 4 }}
+              wrapperCol={{ span: 18 }}
             >
-              <Input size="small" placeholder="粗细" />
+              <Input size="small" placeholder="虚线" />
+            </Form.Item>
+            <Form.Item
+              name={['address', 'province']}
+              label="间隙"
+              labelCol={{ span: 4 }}
+              wrapperCol={{ span: 18 }}
+            >
+              <Input size="small" placeholder="虚线" />
+            </Form.Item>
+          </Input.Group>
+        </Form.Item>
+
+        <Form.Item label="节点" name="layout">
+          <Input.Group className={s.groups}>
+            <Form.Item
+              name={['address', 'province']}
+              label="样式"
+              labelCol={{ span: 4 }}
+              wrapperCol={{ span: 18 }}
+            >
+              <Input size="small" placeholder="样式" />
+            </Form.Item>
+            <Form.Item
+              name={['address', 'province']}
+              label="大小"
+              labelCol={{ span: 4 }}
+              wrapperCol={{ span: 18 }}
+            >
+              <Input size="small" placeholder="样式" />
             </Form.Item>
           </Input.Group>
         </Form.Item>
