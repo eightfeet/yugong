2c9b06f33e42d9b8de211096ae086fff32031c84
eightfeet <bajiao000000@163.com>
2022-09-04 08:23:40 +0800

    增加esBuild提高开发效率

--- config-overrides.js
+++ config-overrides.js
@@ -3,65 +3,72 @@ const path = require('path');
 const lessVariables = require('./config.themeVariable');
 
 const {
-    override,
-    setWebpackOptimizationSplitChunks,
-    fixBabelImports,
-    addLessLoader,
-    setWebpackPublicPath,
-    addWebpackAlias,
+  override,
+  setWebpackOptimizationSplitChunks,
+  fixBabelImports,
+  addLessLoader,
+  setWebpackPublicPath,
+  addWebpackAlias,
 } = require('customize-cra');
 
 const multipleEntry = require('react-app-rewire-multiple-entry')([
-    {
-        entry: 'src/dashboard/index.tsx',
-        template: 'public/yugong.html',
-        outPath: '/dashboard/index.html',
-    },
-    {
-        entry: 'src/index.tsx',
-        outPath: '/index.html',
-    },
+  {
+    entry: 'src/dashboard/index.tsx',
+    template: 'public/yugong.html',
+    outPath: '/dashboard/index.html',
+  },
+  {
+    entry: 'src/index.tsx',
+    outPath: '/index.html',
+  },
 ]);
 
+const rewiredEsbuild = require('react-app-rewired-esbuild');
+
+
 const config = override(
-    fixBabelImports('import', {
-        libraryName: 'antd',
-        libraryDirectory: 'es',
-        style: true, // change importing css to less
-    }),
-    addLessLoader({
-        lessOptions: {
-            javascriptEnabled: true,
-            modifyVars: lessVariables,
-        },
-        additionalData:
-            "@import 'node_modules/antd/lib/style/themes/default.less';",
-    }),
-    setWebpackPublicPath(process.env.REACT_APP_PUBLIC_PATH),
-    addWebpackAlias({
-        '~': path.resolve(__dirname, './src'),
-    }),
-    setWebpackOptimizationSplitChunks({
-        minSize: 30,
-        name: false,
-        cacheGroups: {
-            default: {
-                name: 'common',
-                chunks: 'initial',
-                minChunks: 2, //模块被引用2次以上的才抽离
-                priority: -20,
-            },
-            vendors: {
-                //拆分第三方库（通过npm|yarn安装的库）
-                test: /[\\/]node_modules[\\/]/,
-                name: 'vendor',
-                chunks: 'initial',
-                maxSize: 300000,
-                priority: -10,
-            }
-        },
-    }),
-    multipleEntry.addMultiEntry
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: true, // change importing css to less
+  }),
+  addLessLoader({
+    lessOptions: {
+      javascriptEnabled: true,
+      modifyVars: lessVariables,
+    },
+    additionalData:
+      "@import 'node_modules/antd/lib/style/themes/default.less';",
+  }),
+  setWebpackPublicPath(process.env.REACT_APP_PUBLIC_PATH),
+  addWebpackAlias({
+    '~': path.resolve(__dirname, './src'),
+  }),
+  setWebpackOptimizationSplitChunks({
+    minSize: 30,
+    name: false,
+    cacheGroups: {
+      default: {
+        name: 'common',
+        chunks: 'initial',
+        minChunks: 2, //模块被引用2次以上的才抽离
+        priority: -20,
+      },
+      vendors: {
+        //拆分第三方库（通过npm|yarn安装的库）
+        test: /[\\/]node_modules[\\/]/,
+        name: 'vendor',
+        chunks: 'initial',
+        maxSize: 300000,
+        priority: -10,
+      }
+    },
+  }),
+  multipleEntry.addMultiEntry,
+  rewiredEsbuild({
+    loader: 'tsx',
+    target: 'es2015',
+  }),
 );
 
 module.exports = config;
