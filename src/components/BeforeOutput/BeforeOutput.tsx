992cb10dd5c118ced77027cdb029a18d3d8ce930
eightfeet <bajiao000000@163.com>
2022-10-08 10:08:18 +0800

    fixbug：修改栅格高度

--- src/components/BeforeOutput/BeforeOutput.tsx
+++ src/components/BeforeOutput/BeforeOutput.tsx
@@ -30,7 +30,8 @@ const BeforeOutput: React.FC<Props> = () => {
   const [appDataLocalStoreData] = useLocalStorage('appData', null);
   const [pageDataLocalStoreData] = useLocalStorage('pageData', null);
 
-  const clientRowHeight = parseInt(getResult(`${pageData.rowHeight}`));
+  // 获取并计算栅格行高数据
+  const clientRowHeight = parseInt(getResult(`${pageDataLocalStoreData.rowHeight || pageData.rowHeight}`));
 
   // 用于编辑模式下，观察编辑器通知，设置当前编辑路径
   const sendMessage = usePostMessage((data) => {
